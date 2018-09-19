726031b16ced9a10c62fb03ff43739fa86f7e261
Josh Kassman <jkassman2@gmail.com>
2018-09-19 01:18:28 -0400

    Adding files from meeting on 9-18

---
+++ app/src/main/java/com/jmkassman/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.jmkassman.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button)
+    Button button;
+
+    @BindView(R.id.text_view)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        textView.setAllCaps(true);
+    }
+}
